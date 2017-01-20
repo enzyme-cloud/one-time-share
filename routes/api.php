38b80209fe5daaed339b3c016a190beba12fa77a
Tristan Strathearn <r3oath@gmail.com>
2017-01-21 09:43:32 +1000

    Refactors and limits URL params to strict patterns.

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,9 @@
 <?php
 
 Route::post('secrets', 'SecretsController@store');
-Route::get('secrets/{uuid}/{fetch_code}', 'SecretsController@view');
+
+Route::get('secrets/{uuid}/{fetch_code}', 'SecretsController@view')
+    ->where([
+        'uuid'       => '[a-zA-Z0-9]+',
+        'fetch_code' => '^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$',
+    ]);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/view/{uuid}/{fetch_code}', 'ViewController@index')->name('view');
-Route::get('/how-it-works', 'HowItWorksController@index')->name('how-it-works');
+Route::get('/', 'HomeController@index')
+    ->name('home');
+
+Route::get('/how-it-works', 'HowItWorksController@index')
+    ->name('how-it-works');
+
+Route::get('/view/{uuid}/{fetch_code}', 'ViewController@index')
+    ->where([
+        'uuid'       => '[a-zA-Z0-9]+',
+        'fetch_code' => '^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$',
+    ])
+    ->name('view');
